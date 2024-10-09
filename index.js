00f485010a6af4639c33fd6986f09d0eb14f6ac1
devin <devin.lighgheart@gmail.com>
2024-10-09 11:03:39 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//use alert() to display a message to the user giving them the vault combination
+alert("You have received this message because you havebeen chosen to open an important vault. Here is the secret combination:s 10 - 40 - 39")
+//use const to create three variables that will be the numbers for the combitnation
+
+//const numOne = 11
+//const numTwo = 74
+//const bumThree = 48
+
+let numOne = 11
+let numTwo = 74
+let bumThree = 48
+//use mathematical operators to create euqatoins that result in the three respective numbers of the bault combination
+//set the three vaiables = the three equations, observe error from consy, change to let
+let comboOne = numOne - 1
+let comboTwo = numTwo - 34
+let comboThree = comboTwo - 1
+//ise consol.log to confirm values of the vault code
+console.log(comboOne)
+console.log(comboTwo)
+console.log(comboThree)
